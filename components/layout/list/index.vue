e9fc845445441a64024ffe433bdabc3a3e67d9a3
houbaoguo <1316438321@qq.com>
2020-07-08 00:43:38 +0800

    artice list reconstitute

--- components/layout/list/index.vue
+++ components/layout/list/index.vue
@@ -1,85 +1,54 @@
 <template>
-  <div>
-    <div v-if="listArr.length === 0" class="nomore">
-      暂无数据
-    </div>
-    <ul v-if="!ismoible">
-      <li v-for="(item, index) in listArr" :key="index">
-        <div class="origin">
-          <div class="center">
-            <p :class="item.origin ? (item.origin ==='0' ? 'default' : (item.origin === '1' ? 'transshipment' : 'mixture')) : 'default'">
-              {{ item.origin | transfornOrigin }}
-            </p>
-          </div>
-        </div>
-        <i class="blogpic">
-          <nuxt-link :to="`/article/${item.id}`">
-            <img :src="baseUrl + item.imageUrl" :alt="item.title">
+  <div class="art-item">
+    <div v-for="(item, index) in listArr" :key="index" class="item-box">
+      <div class="thumb-box">
+        <nuxt-link :to="'/article/' + item.id">
+          <img :src="IMG_URL + item.imageUrl" alt="">
+        </nuxt-link>
+      </div>
+      <div class="right-box">
+        <h1 class="title">
+          <nuxt-link :to="'/article/' + item.id">
+            {{ item.title }}
           </nuxt-link>
-        </i>
-        <dl>
-          <dt>
-            <nuxt-link :to="`/tag/${item.tag[0].alias}`" class="tag">
-              {{ item.tag[0].name }}
-            </nuxt-link>
-            <nuxt-link :to="`/article/${item.id}`" tag="h1" class="title">
-              <a>{{ item.title }}</a>
-            </nuxt-link>
-          </dt>
-          <dd>
-            <span class="bloginfo">
-              {{ item.des }}
-            </span>
-            <p class="tab-bottom">
-              <span class="date"><i class="iconfont icon-time" />{{ item.creat_time|parseTime }}</span>
-              <span><i class="iconfont icon-eye" />{{ item.view }}</span>
-              <span><i class="iconfont icon-comment" />{{ item.comment_num }}</span>
-              <span><i class="iconfont icon-heart" />{{ item.like }}</span>
-            </p>
-            <nuxt-link v-if="!ismoible" :to="`/article/${item.id}`" class="ready">
-              查看详情
-            </nuxt-link>
-          </dd>
-        </dl>
-      </li>
-    </ul>
-    <ul v-if="ismoible">
-      <li v-for="(item, index) in listArr" :key="index" @click="goDetails(item.id)">
-        <div class="origin">
-          <div class="center">
-            <p :class="item.origin ? (item.origin ==='0' ? 'default' : (item.origin === '1' ? 'transshipment' : 'mixture')) : 'default'">
-              {{ item.origin | transfornOrigin }}
-            </p>
-          </div>
+        </h1>
+        <div class="desc">
+          <nuxt-link :to="'/article/' + item.id">
+            {{ item.des }}
+          </nuxt-link>
         </div>
-        <dl>
-          <dt>
-            <nuxt-link :to="`/tag/${item.tag[0].alias}`" class="tag">
-              {{ item.tag[0].name }}
-            </nuxt-link>
-            <nuxt-link :to="`/article/${item.id}`" tag="h1" class="title">
-              <a>{{ item.title }}</a>
-            </nuxt-link>
-          </dt>
-          <dd>
-            <span class="bloginfo">
-              {{ item.des }}
-            </span>
-            <p class="tab-bottom">
-              <span class="date"><i class="iconfont icon-time" />{{ item.creat_time|parseTime('{y}-{m}-{d}') }}</span>
-              <span><i class="iconfont icon-eye" />{{ item.view }}</span>
-              <span><i class="iconfont icon-comment" />{{ item.comment_num }}</span>
-              <span><i class="iconfont icon-heart" />{{ item.like }}</span>
-            </p>
-          </dd>
-        </dl>
-      </li>
-    </ul>
+        <div class="bottom-panel">
+          <ul>
+            <li>
+              <svg-icon icon-class="time" class-name="icon" />
+              <span>{{ item.creat_time | parseTime }}</span>
+            </li>
+            <li>
+              <svg-icon icon-class="view" class-name="icon" />
+              <span>{{ item.view }}</span>
+            </li>
+            <li>
+              <svg-icon icon-class="comment" class-name="icon" />
+              <span>{{ item.comment_num }}</span>
+            </li>
+            <li>
+              <svg-icon icon-class="like" class-name="icon" />
+              <span>{{ item.like }}</span>
+            </li>
+            <li>
+              <svg-icon icon-class="listTag" class-name="icon" />
+              <span>{{ item.tag | tagName }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { IMG_URL } from '@/config/constant'
 export default {
   props: {
     listArr: {
@@ -89,7 +58,7 @@ export default {
   },
   data () {
     return {
-      baseUrl: 'http://api.wowmonkey.cn'
+      IMG_URL
     }
   },
   computed: {
@@ -105,183 +74,103 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-  .nomore
-    text-align center
-    padding 20px
-    background rgba(0,0,0,.1)
-    border-radius 6px
-    margin 10px auto
-  ul
-    li
-      position relative
-      overflow hidden
-      padding 20px
-      box-sizing border-box
-      margin-bottom 10px
-      background rgba(255, 255, 255, 0.5)
-      &:hover
-        background rgba(85, 85, 85, 0.1)
-        .blogpic
-          img
-            transform: translateX(-5px)
-      .origin
-        position absolute
-        width 55px;
-        height 55px;
-        top -15px
-        left -15px
-        .center
-          position absolute
-          width 100%
-          top 50%
-          left 50%
-          transform translate(-50%, -50%)
-          p
-            transform: rotate(-45deg)
-            text-align center
-            color #fff
-            font-size 13px
-            &.default
-              background #1AAD19
-            &.transshipment
-              background #ff5e3a
-            &.mixture
-              background #af46f8
-      i.blogpic
-        float left
-        width 28%
-        height 150px
-        margin-right 20px
-        overflow hidden
-        img
-          display inline-block
-          max-width 105%
-          width 105%
-          height: 150px
-          transform: scale(1, 1) rotate(0deg)
-          transition: 0.5s linear
-          &:hover
-            transform: scale(1.1, 1.1) rotate(1deg)
-      dl
-        height 150px;
-        position relative
-        overflow hidden
-        text-overflow ellipsis
-        -webkit-box-orient vertical
-        display -webkit-box
-        -webkit-line-clamp 3
-        width 68%
-        dt
-          a.tag
-            position relative
-            float left
-            padding 3px 6px 3px
-            font-size 12px
-            line-height 14px
-            color #fff
-            vertical-align baseline
-            white-space nowrap
-            background-color #00a2ff
-            margin-right 5px
-            top 4px
-            border-radius 3px
-            &::after
-              content ''
-              position absolute
-              width 0
-              height 0
-              vertical-align: top
-              top 6px
-              right -4px
-              border-left 4px solid #00a2ff
-              border-top 4px solid transparent
-              border-bottom 4px solid transparent
-          .title
-            text-overflow ellipsis
-            white-space nowrap
-            overflow hidden
-            margin-left 5px
-            a
-              position relative
-              text-overflow ellipsis
-              white-space nowrap
-              overflow hidden
-              font-size 20px
-              font-weight normal
-              &::before
-                content: "";
-                position: absolute;
-                width 100%
-                height 2px
-                bottom 0
-                left 0
-                background-color #333
-                visibility hidden
-                transform scaleX(0)
-                transition transform .4s ease-in-out
-              &:hover
-                &::before
-                  transform: scaleX(1)
-                  visibility inherit
-        dd
-          color #989898
-          margin 15px 0
-          display block
-          font-size 14px
-          line-height 22px
-          overflow hidden
-          .tab-bottom
-            font-size 12px
-            position absolute
-            left 0
-            bottom 0
-            span
-              line-height 17px
-              margin-right 20px
-              .iconfont
-                margin-right 5px
-                color #555
-                vertical-align middle
-          .ready
-            position absolute
-            display block
-            background #0077dd
-            color #FFF
-            bottom 0
-            right 0
-            padding 5px 8px
-  @media (max-width: 414px)
-    ul
-      li
-        dl
-          width 100%
-          dd
-            .tab-bottom
-              display flex
-              width 100%
-              span
-                flex 1
-                margin 0
-                text-align center
-              .date
-                flex 1.5
-  @media (max-width: 375px)
-    ul
-      li
-        dl
-          dt
-            .title
-              a{
-                font-size 16px
+<style lang="scss" scoped>
+@import '~assets/scss/mixins.scss';
+  .art-item{
+    .item-box{
+      display: flex;
+      padding: 15px;
+      margin-top: 10px;
+      box-sizing: border-box;
+      box-shadow: 0 0 0 1px #ebebed;
+      border-radius: 2px;
+      background: rgba($color: (#fff), $alpha: 0.6);
+      &:hover{
+        background: rgba($color: (#fff), $alpha: 0.4);
+        .thumb-box{
+          a{
+            img{
+              transform: translateX(-5px)
+            }
+          }
+        }
+      }
+      .thumb-box{
+        a{
+          display: block;
+          width: 195px;
+          height: 130px;
+          overflow: hidden;
+          border-radius: 4px;
+          img{
+            display: inline-block;
+            max-width: 100%;
+            width: 100%;
+            height: 100%;
+            transition: all .5s ease;
+            transform: scale(1, 1) rotate(0deg);
+          }
+        }
+        &:hover{
+          a{
+            img{
+              transform: scale(1.2, 1.2) rotate(1deg)
+            }
+          }
+        }
+      }
+      .right-box{
+        flex: 1;
+        display: flex;
+        flex-direction: column;;
+        margin-left: 16px;
+        .title{
+          a{
+            padding: 2px 0;
+            box-sizing: border-box;
+            font-size: 18px;
+            font-weight: 700;
+            @include ellipsis()
+          }
+        }
+        .desc{
+          flex: 1;
+          margin-top: 6px;
+          line-height: 21px;
+          font-size: 14px;
+          @include ellipsis(3);
+          a{
+            color: #a8abb3;
+          }
+        }
+        .bottom-panel{
+          ul{
+            display: flex;
+            align-items: center;
+            li{
+              margin-right: 15px;
+              font-size: 12px;
+              color: #a8abb3;
+              cursor: pointer;
+              &:first-child{
+                .icon{
+                  margin-top: -1px;
+                }
               }
-  @media (max-width: 320px)
-    ul
-      li
-        dl
-          dt
-            .title
-              a{
-                font-size 14px
+              &:last-child{
+                @include ellipsis(1);
+                .icon{
+                  font-size: 14px;
+                }
               }
+              .icon{
+                font-size: 13px;
+                margin-right: 2px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
